e66a1e91e62c4bb79d92f217bdb8793dbfd59a28
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-02 01:02:06 +0900

    update

---
+++ src/Algorithm/Basic/_10171.java
@@ -0,0 +1,12 @@
+package Algorithm.Basic;
+
+public class _10171 {
+    public static void main(String[] args) {
+        System.out.println(
+                "\\    /\\\n" +
+                        " )  ( ')\n" +
+                        "(  /  )\n" +
+                        " \\(__)|"
+        );
+    }
+}
---
+++ src/Algorithm/Basic/_2588.java
@@ -0,0 +1,20 @@
+package Algorithm.Basic;
+
+import java.util.Scanner;
+
+public class _2588 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int b1 = ((b / 100) % 10);
+        int b10 = ((b / 10) % 10);
+        int b100 = (b % 10);
+
+        System.out.println(a * b100);
+        System.out.println(a * b10);
+        System.out.println(a * b1);
+        System.out.println(a * b);
+    }
+}

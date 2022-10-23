1c2a8511abe8cf1fda44f3d825075b9b20624b76
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-10-24 02:25:11 +0900

    update

--- src/Algorithm/Basic/CheckJavaSyntax/_03.java
+++ src/Algorithm/Basic/CheckJavaSyntax/_03.java
@@ -20,29 +20,26 @@ public class _03 {
     }
 
     private static void insertionSort(int[] arr) {
-
         int i = 0, j = 0;
         int temp = 0;
 
-        for (i = 1; i < arr.length; i++) {
+        for (i = 0; i < arr.length; i++) {
             temp = arr[i];
             j = i;
-            while ((j > 0) && arr[j - 1] > temp) {
-                arr[j] = arr[j - 1];
-                j -= 1;
+            while ((j>0) && (arr[j-1] > temp)) {
+                arr[j] = arr[j-1];
+                j = j - 1;
             }
             arr[j] = temp;
 
-            System.out.println("반복" + i);
-
+            System.out.println("반복 " + i);
             printSort(arr);
         }
     }
 
     private static void printSort(int[] arr) {
-        int i = 0;
-        for (i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + "\t");
+        for (int j : arr) {
+            System.out.println(j + "\t");
         }
         System.out.println();
     }
